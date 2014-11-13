870eef2a9b22692af7b3e291635a64857c65b78b
Calla <callapatel@gmail.com>
2014-11-12 17:09:13 -0800

    some styles

--- adalist.js
+++ adalist.js
@@ -7,7 +7,7 @@ function hope(e) {
   e.preventDefault();
 
 
-  $veganOreos = $("#list").append("<li><span>"+ $("#task").val() +"</span></li>");
+  $veganOreos = $("#list").append("<li>"+ $("#task").val() +"</li>");
 
   $("#task").val("");
   var oreo = (($("#list").children().length % 2 === 0 )?  ($("#list :last-child").css("background-color", "pink")) : ($("#list :last-child").css("background-color", "grey")));
